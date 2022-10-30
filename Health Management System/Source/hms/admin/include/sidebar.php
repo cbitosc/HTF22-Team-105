9b831ba4442ca8113eef372f29cfcc5198bae290
Anuraag-8 <anuraag.cbit@gmail.com>
2022-10-30 13:13:33 +0530

    add source code

---
+++ Health Management System/Source/hms/admin/include/sidebar.php
@@ -0,0 +1,207 @@
+<div class="sidebar app-aside" id="sidebar">
+				<div class="sidebar-container perfect-scrollbar">
+
+<nav>
+						
+						<div class="navbar-title">
+							<span>Main Navigation</span>
+						</div>
+						<ul class="main-navigation-menu">
+							<li>
+								<a href="dashboard.php">
+									<div class="item-content">
+										<div class="item-media">
+											<i class="ti-home"></i>
+										</div>
+										<div class="item-inner">
+											<span class="title"> DASHBOARD </span>
+										</div>
+									</div>
+								</a>
+							</li>
+							<li>
+								<a href="javascript:void(0)">
+									<div class="item-content">
+										<div class="item-media">
+											<i class="ti-user"></i>
+										</div>
+										<div class="item-inner">
+											<span class="title"> DOCTORS </span><i class="icon-arrow"></i>
+										</div>
+									</div>
+								</a>
+								<ul class="sub-menu">
+									<li>
+										<a href="doctor-specilization.php">
+											<span class="title"> DOCTOR SPECIALIZATION </span>
+										</a>
+									</li>
+									<li>
+										<a href="add-doctor.php">
+											<span class="title"> ADD DOCTOR</span>
+										</a>
+									</li>
+									<li>
+										<a href="Manage-doctors.php">
+											<span class="title"> MANAGE DOCTORS </span>
+										</a>
+									</li>
+									
+								</ul>
+								</li>
+
+				<li>
+								<a href="javascript:void(0)">
+									<div class="item-content">
+										<div class="item-media">
+											<i class="ti-user"></i>
+										</div>
+										<div class="item-inner">
+											<span class="title"> USERS </span><i class="icon-arrow"></i>
+										</div>
+									</div>
+								</a>
+								<ul class="sub-menu">
+									
+									<li>
+										<a href="manage-users.php">
+											<span class="title"> MANAGE USERS </span>
+										</a>
+									</li>
+									
+								</ul>
+								</li>
+								<li>
+								<a href="javascript:void(0)">
+									<div class="item-content">
+										<div class="item-media">
+											<i class="ti-user"></i>
+										</div>
+										<div class="item-inner">
+											<span class="title"> PATIENTS </span><i class="icon-arrow"></i>
+										</div>
+									</div>
+								</a>
+								<ul class="sub-menu">
+									
+									<li>
+										<a href="manage-patient.php">
+											<span class="title"> MANAGE PATIENTS </span>
+										</a>
+									</li>
+									
+								</ul>
+								</li>	
+
+<li>
+								<a href="appointment-history.php">
+									<div class="item-content">
+										<div class="item-media">
+											<i class="ti-file"></i>
+										</div>
+										<div class="item-inner">
+											<span class="title"> APPONITMENT HISTORY </span>
+										</div>
+									</div>
+								</a>
+							</li>
+
+
+
+				<li>
+								<a href="javascript:void(0)">
+									<div class="item-content">
+										<div class="item-media">
+											<i class="ti-files"></i>
+										</div>
+										<div class="item-inner">
+											<span class="title"> CONTACTUS QUERIES </span><i class="icon-arrow"></i>
+										</div>
+									</div>
+								</a>
+								<ul class="sub-menu">
+									
+									<li>
+										<a href="unread-queries.php">
+											<span class="title"> UREAD QUERIES </span>
+										</a>
+									</li>
+
+									<li>
+										<a href="read-query.php">
+											<span class="title"> READ QUERY </span>
+										</a>
+									</li>
+									
+								</ul>
+								</li>
+
+
+
+	<li>
+								<a href="doctor-logs.php">
+									<div class="item-content">
+										<div class="item-media">
+											<i class="ti-list"></i>
+										</div>
+										<div class="item-inner">
+											<span class="title"> DOCTOR LOG SESSIONS</span>
+										</div>
+									</div>
+								</a>
+							</li>		
+
+
+
+							<li>
+								<a href="user-logs.php">
+									<div class="item-content">
+										<div class="item-media">
+											<i class="ti-list"></i>
+										</div>
+										<div class="item-inner">
+											<span class="title"> USER LOGIN SESSIONS </span>
+										</div>
+									</div>
+								</a>
+							</li>						
+				<li>
+								<a href="javascript:void(0)">
+									<div class="item-content">
+										<div class="item-media">
+											<i class="ti-files"></i>
+										</div>
+										<div class="item-inner">
+											<span class="title"> REPORTS </span><i class="icon-arrow"></i>
+										</div>
+									</div>
+								</a>
+								<ul class="sub-menu">
+									
+									<li>
+										<a href="between-dates-reports.php">
+											<span class="title">B/w dates reports </span>
+										</a>
+									</li>
+
+									
+									
+								</ul>
+								<li>
+								<a href="patient-search.php">
+									<div class="item-content">
+										<div class="item-media">
+											<i class="ti-search"></i>
+										</div>
+										<div class="item-inner">
+											<span class="title"> PATIENT SEARCH </span>
+										</div>
+									</div>
+								</a>
+							</li>
+								</li>
+
+						</ul>
+					</nav>
+					</div>
+			</div>
